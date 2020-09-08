e20e5866d8c114ddee4d3e43bc7cee886450f6a9
Pablo Gonzalez <pabgon0@gmail.com>
2020-09-08 23:13:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your functions here
+function map(src, cback) {
+  let r = []
+
+  for (let i = 0; i < src.length; i++) {
+    let theElement = src[i]
+    r.push(cback(theElement))
+  }
+
+  return r;
+}
+
+function reduce(src, cback, start){
+  let r = (!!start) ? start : src[0]
+  let i = (!!start) ? 0 : 1
+
+  for (; i < src.length; i++) {
+    r = cback(src[i], r)
+  }
+
+  return r;
+}
